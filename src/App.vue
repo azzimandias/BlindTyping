be26c2cb60347927217ac5ef58c0019fd29a39df
Andrew Sharanda <azzimandias@gmail.com>
2021-05-15 16:43:59 +0300

    Add basic markup and Start/Stop button

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,131 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <nav class="controls">
+      <div class="controls__body">
+        <h2 class="controls__title">
+          Blind Typing
+        </h2>
+        <StartButton
+            @changeIsTraining="changeIsTraining()"
+        />
+      </div>
+    </nav>
+
+    <div class="training">
+      <div class="training__body">
+        <div class="training__info-body" v-if="!isTraining">
+          <h2 class="training__info-title">
+            Blind typing training web application.<br/>
+            Implemented using the Vue framework.<br/>
+            Click 'Start training' to run.<br/>
+          </h2>
+        </div>
+
+        <div class="training__start-body" v-else>
+
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import StartButton from "@/components/StartButton";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    StartButton,
+  },
+  data() {
+    return {
+      isTraining: false,
+    }
+  },
+  methods: {
+    changeIsTraining() {
+      this.isTraining = !this.isTraining;
+    }
+  },
+  computed: {
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    background-color: white;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    border: none;
+  }
+
+  .app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+
+  .controls {
+    width: 100%;
+    margin: 10px 0;
+    //border: 1px solid black;
+  }
+
+  .controls__body {
+    max-width: 800px;
+    height: 70px;
+    margin: 0 auto;
+    padding: 10px 30px;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    border: 1px solid black;
+    border-radius: 5px;
+  }
+
+  .training__body {
+    max-width: 800px;
+    height: 700px;
+    margin: 0 auto;
+    padding: 10px 30px;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    border: 1px solid black;
+    border-radius: 5px;
+  }
+
+  .training__info-body  {
+    position: relative;
+    max-width: 75%;
+    margin: 0 auto 30%;
+    padding: 0 10px;
+    //border: 1px solid black;
+  }
+
+  .training__info-body::after {
+    content: '';
+    position: absolute;
+    left: calc(50% - 200px);
+    bottom: -20%;
+    width: 400px;
+    height: 5px;
+    background: #6D7278;
+  }
+
+  .training__start-body {
+    width: 100%;
+    height: 100%;
+    border: 1px solid black;
+  }
 </style>
---
+++ src/components/StartButton.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <button
+        class="SB__body-1"
+        v-if="!isClicked"
+        @mouseup="changeIsTraining()"
+    >▶ Start training
+    </button>
+    <button
+        class="SB__body-2"
+        v-else
+        @mouseup="changeIsTraining()"
+    >◼ Stop
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "StartButton",
+  data() {
+    return {
+      isClicked: false,
+    }
+  },
+  methods: {
+    changeIsTraining() {
+      this.isClicked = !this.isClicked;
+      this.$emit('changeIsTraining');
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .SB__body-1 {
+    width: 150px;
+    height: 40px;
+    background-color: aquamarine;
+    font-size: 15px;
+    font-weight: 600;
+    border: 1px solid black;
+    border-radius: 20px;
+  }
+
+  .SB__body-2 {
+    width: 150px;
+    height: 40px;
+    background-color: tomato;
+    font-size: 15px;
+    font-weight: 600;
+    border: 1px solid black;
+    border-radius: 20px;
+  }
+</style>
