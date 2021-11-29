69a0f69a0304e3263bbcd55d0ff77208349932b5
Andrew Sharanda <azzimandias@gmail.com>
2021-11-29 13:34:35 +0300

    Add build for heroku

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const serveStatic = require('serve-static');
+const path = require('path');
+
+const app = express();
+
+app.use('/BlindTyping/', serveStatic(path.join(__dirname, '/dist')));
+
+app.get(/.*/,  (req, res) => {
+    res.sendFile(path.join(__dirname, '/dist/index.html'));
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port);
+console.log(`app is listening on port: ${port}`);
